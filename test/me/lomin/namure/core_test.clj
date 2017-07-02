600cbe070ed990ce9f6d5770bf3c0078cde128a0
lomin <happylomin@gmail.com>
2017-07-02 13:23:09 +0200

    Green: converts-namespace-of-var-to-list-test

--- test/me/lomin/namure/core_test.clj
+++ test/me/lomin/namure/core_test.clj
@@ -1,7 +1,19 @@
 (ns me.lomin.namure.core-test
   (:require [clojure.test :refer :all]
-            [me.lomin.namure.core :as namure]))
+            [me.lomin.namure.core :as namure]
+            [clojure.edn :as edn]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn read-namespace-of [{file-str :file}]
+  (with-open [rdr (new java.io.PushbackReader
+                       (clojure.java.io/reader (clojure.java.io/resource file-str)))]
+    (vec (take-while some?
+                     (repeatedly (partial read
+                                          {:eof nil}
+                                          rdr))))))
+
+(defmacro make-tree [sym]
+  `(read-namespace-of (meta (var ~sym))))
+
+(deftest converts-namespace-of-var-to-list-test
+  (is (<= 4
+          (count (read-namespace-of {:file "me/lomin/namure/core_test.clj"})))))
