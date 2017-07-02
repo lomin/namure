092a1126a22c0ee58cbbe838ed58554d54a2ca31
lomin <happylomin@gmail.com>
2017-07-02 13:44:55 +0200

    Red: finds-all-user-functions-test

--- test/me/lomin/namure/core_test.clj
+++ test/me/lomin/namure/core_test.clj
@@ -17,3 +17,24 @@
 (deftest converts-namespace-of-var-to-list-test
   (is (<= 4
           (count (read-namespace-of {:file "me/lomin/namure/core_test.clj"})))))
+
+(defn sym-xs->tree [param1]
+  {:functions nil})
+
+(deftest finds-all-user-functions-test
+  (is (= [read-namespace-of]
+         (:functions (sym-xs->tree '((ns
+                                       me.lomin.namure.core-test
+                                       (:require
+                                         [clojure.test :refer :all]
+                                         [me.lomin.namure.core :as namure]
+                                         [clojure.edn :as edn]))
+                                      (defn
+                                        read-namespace-of
+                                        [{file-str :file}]
+                                        (with-open
+                                          [rdr
+                                           (new
+                                             java.io.PushbackReader
+                                             (clojure.java.io/reader (clojure.java.io/resource file-str)))]
+                                          (vec (take-while some? (repeatedly (partial read {:eof nil} rdr))))))))))))
